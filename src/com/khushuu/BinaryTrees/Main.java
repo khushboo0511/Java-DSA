69211ca33699f94c706573a782dd23972a5b0e6b
Khushboo0511 <khushboochaudhary0511@gmail.com>
2024-01-20 00:07:01 +0530

    Binary Trees

--- src/com/khushuu/BinaryTrees/Main.java
+++ src/com/khushuu/BinaryTrees/Main.java
@@ -2,15 +2,22 @@ package com.khushuu.BinaryTrees;
 
 import java.util.Scanner;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
 //        Scanner scanner = new Scanner(System.in);
 //        BinaryTree tree = new BinaryTree();
 //        tree.populate(scanner);
 //        tree.prettyDisplay();
-        BST tree = new BST();
-        int[] nums = { 5, 2, 7, 1, 4, 6, 9, 8, 3, 10 };
-        tree.populate(nums);
-        tree.display();
+//        BST tree = new BST();
+//        int[] nums = { 5, 2, 7, 1, 4, 6, 9, 8, 3, 10 };
+//        tree.populate(nums);
+//        tree.display();
+
+        AVL tree = new AVL();
+
+        for (int i=0; i<500; i++) {
+            tree.insert(i);
+        }
+        System.out.println(tree.height());
     }
 }
